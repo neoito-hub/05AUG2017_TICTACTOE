6e0c723af70fc03511eb17c74eaeb74e1405ee71
Faiz M Haneef <faizmh@neoito.com>
2017-08-05 12:09:55 +0530

    comments

---
+++ main.js
@@ -0,0 +1,22 @@
+// start the game
+
+// check which are the 2 teams
+
+// register their respective functions
+
+// decide game mode
+
+// decide first turn
+
+// loop start
+// team 1 chance
+
+// validate TTT arr
+
+// render TTT
+
+// decide continue or winner
+
+// register score
+
+// loop end
